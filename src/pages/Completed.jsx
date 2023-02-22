0b2d16ea660bf3ece44f2e72634634c105d1cbed
Roman <coldshein@gmail.com>
2023-02-22 22:45:27 +0200

    updated

--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 import {useSelector} from 'react-redux';
 import CompletedTodo from '../components/CompletedTodo'
 
@@ -8,17 +8,20 @@ const Completed = () => {
     const todos = useSelector((state) => state.todo.todos);
 
     const completedTodos = todos.filter((item) => item.completed === true );
-    console.log(completedTodos)
     return ( 
         <>
         <div className="completed-wrapper">
-            <h1 className="completed-title">Completed tasks:</h1>
+            <h1 className="completed-title">{
+                completedTodos.length > 0 ? 'Completed tasks' : 'There are not completed todos' 
+            }</h1>
             <div className="completed-list">
                {
                 completedTodos.map((item) => <CompletedTodo key={item.id} {...item}/> )
                }
             </div>
+            <Link to="/" className='goback'>Go back</Link>
         </div>
+        
         </>
      );
 }
